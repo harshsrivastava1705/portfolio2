4f01cd3daaf166b080611f3fe88fb4d8ed096794
harshsrivastava1705 <69566573+harshsrivastava1705@users.noreply.github.com>
2021-12-26 23:32:10 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -55,7 +55,8 @@ emailjs.init("user_PrLzDYPz3tYaFhKo5kmsZ");
 })();
 </script>
 
-
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 </head>
 
 
